e042d4cad89e2faaa331f1f338b7693d33d72d28
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-09-22 11:27:38 -0400

    Add comments to script.js, add README

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+// Assignment Code (code provided with project)
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input (code provided with project)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,15 +10,16 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button (code provided with project)
 generateBtn.addEventListener("click", writePassword);
 
 
+//my code below
 function generatePassword(){
-  var promptText =["Please enter a password length:", "Do you want lowercase letters?", "Do you want uppercase letters?",
-                    "Do you want numbers?", "Do you want special characters?" ];
 
+  // Array to store input from user prompts for reference.
   var promptAnswers = [];     
+
 
   var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
@@ -31,9 +32,18 @@ function generatePassword(){
   var specialCharaters = [  '@',  '%',  '+',  '\\',  '/',  "'",  '!',  '#',  '$',  '^',  '?',  ':',  ',',  ')',
     '(',  '}',  '{',  ']',  '[',  '~',  '-',  '_',  '.'];
 
+  //all all chars to array in their arrays (matrix) to be stored for later use
   var allChars = ["Start"];
   allChars.push(lowerCasedCharacters, upperCasedCharacters, numericCharacters, specialCharaters);
 
+  //store all prompts in arary to easily access or change, will be called from the array when the prompt/confirm is called
+  var promptText =["Please enter a password length:", "Do you want lowercase letters?", "Do you want uppercase letters?",
+  "Do you want numbers?", "Do you want special characters?" ];
+
+  //allChars and promptText are linked in the order they are listed. if the order in one changes, the other in the other should
+  //change to match, for example, if prompt is remv or if an option is added.
+
+  //will hold chars that will be used to generate the password based on the user selections
   var validChars = [];
 
   var keepGoing = false;
@@ -44,14 +54,17 @@ function generatePassword(){
 
   do {   
 
+    //check if this is the first question, since the first question requires an actuall input we want to 
+    //use prompt, all other questions will be using a confirm box, this will also call the input validation 
     if(question == 0){
       answer = prompt(promptText[question]);
+      //verify if answer is actually a number and within the range of numbers required
       if(!isNaN(answer) && answer >= 8 && answer <= 128){
-        promptAnswers[question] = Math.trunc(answer); 
-        keepGoing = true;
+        promptAnswers[question] = Math.trunc(answer); //will remove and point of presicion from answer as 'isNaN' will show a decimal as true.
+        keepGoing = true; //conf selection is good and validate the loop check indicator
         question++;
       }
-      else{
+      else{ //if the prompt is no valid ensure loop validator is false to break loop.
         keepGoing = false;
         alert("Please enter a valid length");
       }
@@ -59,15 +72,18 @@ function generatePassword(){
     else{
       answer = confirm(promptText[question])
       if (answer){
+        //if user conf wants to use a char set, add those chars to the array that will hold all selected chars for the password gen
+        //if user does not confirm will go to the next question with no changes to the chars to be used for the password gen
         validChars = validChars.concat(allChars[question]);
         promptAnswers[question] = answer; 
-       }
-       else{
+      }
+      else{
         promptAnswers[question] = answer; 
-       } 
+      } 
       question++;
     }
 
+    //ensures break from loop once all questions have been answered
     if (question > promptText.length-1) {
         keepGoing = false;
     }
@@ -75,14 +91,16 @@ function generatePassword(){
     
   } while (keepGoing);
 
+  //based on user selected password length, define max limit for use in random number
+  //must be the length minus one because this will be the upper bounds of the array and to avoid an out of bounds error
   var max = validChars.length-1;
 
+  //create the number of chars based the length of the password the user desired
+  //use a random index from the 'validChars' which contains all the chars selected by the user
   for(let x = 0; x < promptAnswers[0]; x++){
     password += validChars[random(0,max)];
   } 
   
-  console.log(promptAnswers);
-  console.log(validChars);
   return password;
 
 }
